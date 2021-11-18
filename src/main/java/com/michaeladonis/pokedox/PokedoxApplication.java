cbb68b42db0e517e08d202fbb00e0d9f4748b789
Mike <blakesmike12@gmail.com>
2021-11-18 02:56:42 +0100

    Brand-new spring.io project with all required dependencies

---
+++ src/main/java/com/michaeladonis/pokedox/PokedoxApplication.java
@@ -0,0 +1,13 @@
+package com.michaeladonis.pokedox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedoxApplication.class, args);
+    }
+
+}
